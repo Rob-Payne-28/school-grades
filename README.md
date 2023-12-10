939c15a6f5ac41eb97258246bc44f15704a189e4
Rob Payne <robert.h.payne28.mil@army.mil>
2023-12-10 12:55:42 -0600

    Add README and add logging and error handling

---
+++ README.md
@@ -0,0 +1,56 @@
+# Grade Fetch Script
+#### https://github.com/Rob-Payne-28/school-grades
+## Description
+This script automatically logs into our school's website and extracts grade information. It handles authentication and parses HTML content to extract IPR data.
+
+## Setup Instructions
+### Install Python:
+Ensure Python 3 is installed on your system. This script has been tested with Python 3.9. If not already installed, you can download it from python.org or install it via Homebrew on macOS:
+
+```shell
+brew install python
+```
+
+### Install dependencies
+This script uses libraries for https session management and parsing returned data. If you don't have them installed, you will need to install them.
+
+```shell
+# Used to handle session information
+pip3 install requests
+# Used to parse returned webpages
+pip3 install beautifulsoup4
+# Used to pull in environment variables
+pip3 install python-dotenv
+```
+### Clone the Repository:
+Clone the script to your local machine.
+
+```shell
+# Via https
+git clone https://github.com/Rob-Payne-28/school-grades.git
+```
+
+### Environment Variables:
+Create a .env file (if one doesn't already exist) in the same directory as your script to store sensitive information like URLs and login credentials. Refer to the sample .env template below.
+
+```shell
+touch .env
+echo "LOGIN_PAGE_URL=https://example.com/login" >> .env
+echo "LOGIN_ACTION_URL=https://example.com/login-action" >> .env
+echo "USERNAME=your_username" >> .env
+echo "PASSWORD=your_password" >> .env
+echo "I_FRAME_URL=https://example.com/iframe-base-url" >> .env
+echo "USER_AGENT=YourUserAgentStringHere" >> .env
+```
+
+## Run the Script:
+With everything set up, run the script:
+
+```shell
+python fetch-grades.py
+```
+
+## Troubleshooting
+- If you encounter issues, check the grades_script.log file for error messages.
+- Ensure that the .env file is correctly formatted and contains the necessary information.
+
--- fetch-grades.py
+++ fetch-grades.py
@@ -3,93 +3,103 @@ import json
 import os
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+import logging
+
+# Setup logging
+logging.basicConfig(filename='grades_script.log', level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
 
 load_dotenv()
 
-login_page_url = os.environ.get('LOGIN_PAGE_URL')
-login_action_url = os.environ.get('LOGIN_ACTION_URL')
-username = os.environ.get('USERNAME')
-password = os.environ.get('PASSWORD')
-baseUrl = os.environ.get('I_FRAME_URL')
-user_agent_string = os.environ.get('USER_AGENT')
+try:
+    login_page_url = os.environ.get('LOGIN_PAGE_URL')
+    login_action_url = os.environ.get('LOGIN_ACTION_URL')
+    username = os.environ.get('USERNAME')
+    password = os.environ.get('PASSWORD')
+    baseUrl = os.environ.get('I_FRAME_URL')
+    user_agent_string = os.environ.get('USER_AGENT')
 
-session = requests.Session()
+    session = requests.Session()
+    session.headers.update({'User-Agent': user_agent_string})
 
-session.headers.update({
-    'User-Agent': user_agent_string,
-})
+    response = session.get(login_page_url)
+    response.raise_for_status()
 
-response = session.get(login_page_url)
-soup = BeautifulSoup(response.text, 'html.parser')
-token = soup.find('input', {'name': '__RequestVerificationToken'}).get('value')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    token = soup.find('input', {'name': '__RequestVerificationToken'}).get('value')
 
-login_data = {
-    '__RequestVerificationToken': token,
-    'SCKTY00328510CustomEnabled': 'False',
-    'SCKTY00436568CustomEnabled': 'False',
-    'Database': '10',
-    'VerificationOption': 'UsernamePassword',
-    'LogOnDetails.UserName': username,
-    'tempUN': '',
-    'tempPW': '',
-    'LogOnDetails.Password': password
-}
+    login_data = {
+        '__RequestVerificationToken': token,
+        'SCKTY00328510CustomEnabled': 'False',
+        'SCKTY00436568CustomEnabled': 'False',
+        'Database': '10',
+        'VerificationOption': 'UsernamePassword',
+        'LogOnDetails.UserName': username,
+        'tempUN': '',
+        'tempPW': '',
+        'LogOnDetails.Password': password
+    }
 
-# Add additional headers for the POST request
-headers = {
-    'User-Agent': user_agent_string,
-    'Referer': login_page_url,
-}
+    headers = {
+        'User-Agent': user_agent_string,
+        'Referer': login_page_url,
+    }
 
-print("\n\n\n******************************************")
-print("Getting updated grades...")
-print("******************************************\n\n\n")
+    logging.info("Attempting to authenticate...")
+    login_response = session.post(login_action_url, data=login_data, headers=headers)
+    login_response.raise_for_status()
+    soup = BeautifulSoup(login_response.text, 'html.parser')
 
-login_response = session.post(login_action_url, data=login_data, headers=headers)
-soup = BeautifulSoup(login_response.text, 'html.parser')
-
-iframe = soup.find('iframe', id='sg-legacy-iframe')
-iframe_url = iframe['src'] if iframe else None
-
-if iframe_url:
-    if not iframe_url.startswith('http'):
-        iframe_url = baseUrl + iframe_url
-
-    iframe_response = session.get(iframe_url)
-    iframe_content = iframe_response.text
-
-    soup = BeautifulSoup(iframe_content, 'html.parser')
-    table = soup.find('table', {'id': 'plnMain_dgIPR'})
-
-    if table:
-        header_row = table.find('thead') or table.find('tr')
-        headers = [header.text.strip() for header in header_row.find_all(['th', 'td'])]
-
-        data_list = []
-        rows = table.find_all('tr')[1:]
-        for row in rows:
-            cols = row.find_all('td')
-            print("Cols:", [col.text.strip() for col in cols])  # Check the columns in each row
-
-            row_data = {header: col.text.strip() for header, col in zip(headers, cols)}
-            data_list.append(row_data)
-
-        comment_legend_table = soup.find('table', {'id': 'plnMain_dgCommentLegend'})
-        comment_legend = {}
-        if comment_legend_table:
-            for row in comment_legend_table.find_all('tr')[1:]:  # Skipping the header
-                code, description = [td.text.strip() for td in row.find_all('td')]
-                comment_legend[code] = description
-
-        for row_data in data_list:
-            for key in ['CM1', 'CM2']:
-                if row_data[key] in comment_legend:
-                    row_data[key] = comment_legend[row_data[key]]
-                    
-        json_data = json.dumps(data_list, indent=4)
-        print(json_data)
+    if login_response.ok:
+        logging.info("Authentication successful.")
     else:
-        print("Table not found")
-else:
-    print("iframe not found")
+        logging.error(f"Authentication failed. Status code: {login_response.status_code}")
 
+    logging.info("Getting updated grades...")
+
+    iframe = soup.find('iframe', id='sg-legacy-iframe')
+    iframe_url = iframe['src'] if iframe else None
+
+    if iframe_url:
+        if not iframe_url.startswith('http'):
+            iframe_url = baseUrl + iframe_url
+
+        iframe_response = session.get(iframe_url)
+        iframe_response.raise_for_status()
+
+        soup = BeautifulSoup(iframe_response.text, 'html.parser')
+        table = soup.find('table', {'id': 'plnMain_dgIPR'})
+
+        if table:
+            header_row = table.find('thead') or table.find('tr')
+            headers = [header.text.strip() for header in header_row.find_all(['th', 'td'])]
+
+            data_list = []
+            rows = table.find_all('tr')[1:]
+            for row in rows:
+                cols = row.find_all('td')
+                row_data = {header: col.text.strip() for header, col in zip(headers, cols)}
+                data_list.append(row_data)
+
+            comment_legend_table = soup.find('table', {'id': 'plnMain_dgCommentLegend'})
+            comment_legend = {}
+            if comment_legend_table:
+                for row in comment_legend_table.find_all('tr')[1:]:  # Skipping the header
+                    code, description = [td.text.strip() for td in row.find_all('td')]
+                    comment_legend[code] = description
+
+            for row_data in data_list:
+                for key in ['CM1', 'CM2']:
+                    if row_data[key] in comment_legend:
+                        row_data[key] = comment_legend[row_data[key]]
+
+            json_data = json.dumps(data_list, indent=4)
+            print(json_data)
+        else:
+            logging.error("Table not found")
+    else:
+        logging.error("iframe not found")
+
+except requests.exceptions.RequestException as e:
+    logging.error(f"Request error: {e}")
+except Exception as e:
+    logging.error(f"An unexpected error occurred: {e}")
